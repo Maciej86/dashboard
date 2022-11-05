dc6d7c74c1e8f1625f46d3a05b77282179a17faa
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-05 01:22:45 +0100

    Animation section

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,16 +1,20 @@
+import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import { Bar } from "../features/Bar";
+import { selectToggleNavState } from "../features/Bar/sliceBar";
 import { Navigation } from "../features/Navigation";
+import { Bar } from "../features/Bar";
 import { Section, Wrapper } from "./styled";
 import { themeDefault } from "./theme";
 
 export const App = () => {
+  const toggleNav = useSelector(selectToggleNavState);
+
   return (
     <ThemeProvider theme={themeDefault}>
       <Wrapper>
         <Navigation />
         <Bar />
-        <Section>Tutaj treść całego pulpitu</Section>
+        <Section $toggleNav={toggleNav}>Tutaj treść całego pulpitu</Section>
       </Wrapper>
     </ThemeProvider>
   );
--- src/core/styled.ts
+++ src/core/styled.ts
@@ -1,15 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { size } from "./theme";
+interface AnimationSection {
+  $toggleNav: boolean;
+}
 
 export const Wrapper = styled.main`
   height: 100%;
   overflow: auto;
 `;
 
-export const Section = styled.section`
+export const Section = styled.section<AnimationSection>`
   height: calc(100% - ${size.heightBar}px);
   background: ${({ theme }) => theme.backgroundApp};
   margin-top: ${size.heightBar}px;
   margin-left: 300px;
   padding: 20px;
+  transition: margin-left 0.5s, width 0.5s;
+
+  ${({ $toggleNav }) =>
+    $toggleNav &&
+    css`
+      margin-left: 0;
+      width: 100%;
+    `}
 `;
