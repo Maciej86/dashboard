b83a5b944eecef50fb81fd1e6d02b8dddf7332d3
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-06 21:40:04 +0100

    Styled button

--- src/common/Button/styled.ts
+++ src/common/Button/styled.ts
@@ -1,12 +1,27 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  padding: 7px 23px;
-  font-size: 16px;
+  padding: 9px 27px;
+  font-size: 17px;
   border: none;
+  transition: background 0.5s;
   cursor: pointer;
 `;
 
-export const ButtonOk = styled(Button)``;
+export const ButtonOk = styled(Button)`
+  background: ${({ theme }) => theme.color.buttonConfirmBg};
+  color: ${({ theme }) => theme.color.colorTextWhite};
 
-export const ButtonAnnul = styled(Button)``;
+  &:hover {
+    background: ${({ theme }) => theme.color.buttonConfirmBgHover};
+  }
+`;
+
+export const ButtonAnnul = styled(Button)`
+  background: ${({ theme }) => theme.color.buttonCancelBg};
+  color: ${({ theme }) => theme.color.colorTextBasic};
+
+  &:hover {
+    background: ${({ theme }) => theme.color.buttonCancelBgHover};
+  }
+`;
--- src/core/theme.ts
+++ src/core/theme.ts
@@ -6,10 +6,11 @@ const color = {
   pickledBluewood_300: "rgb(35, 51, 66)",
   gray_100: "rgb(235, 237, 239)",
   gray_400: "rgb(189, 189, 189)",
+  gray_500: "rgb(149, 149, 149)",
   gray_600: "rgb(109, 109, 109)",
   gray_900: "rgb(69, 69, 69)",
   jungleGreen_100: "rgb(46, 184, 92)",
-  jungleGreen_200: "rgb(43, 171, 85)",
+  jungleGreen_200: "rgb(40, 161, 80)",
 };
 
 const size = {
@@ -42,6 +43,8 @@ export const themeDefault = {
     boxShadow: color.gray_900,
     buttonConfirmBg: color.jungleGreen_100,
     buttonConfirmBgHover: color.jungleGreen_200,
+    buttonCancelBg: color.gray_400,
+    buttonCancelBgHover: color.gray_500,
   },
   size: {
     ...size,
