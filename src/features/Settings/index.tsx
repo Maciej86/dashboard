176e76b241d33f1ecd2e97ca1f539e7a8f474e2f
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-15 22:36:32 +0100

    Added action settings change name

--- src/features/Settings/index.tsx
+++ src/features/Settings/index.tsx
@@ -1,21 +1,45 @@
-import { useSelector } from "react-redux";
-import { selectNameState } from "../Name/sliceName";
+import { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addNewName, selectNameState } from "../Name/sliceName";
 import { ButtonConfirm } from "../../common/Button/styled";
-import { InputText } from "../../common/Input/styled";
 import { Description, Tile, TileTitle } from "../../common/Tile/styled";
-import { Form } from "./styled";
+import { Form, InputName } from "./styled";
 
 export const Settings = () => {
-  const userName = useSelector(selectNameState);
+  const userNameState = useSelector(selectNameState);
+  const userName = useRef<HTMLInputElement>(null);
+  const [validateName, setValidateName] = useState(false);
+  const dispatch = useDispatch();
+
+  const onFromSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (userName.current !== null) {
+      const trimUserName = userName.current.value.trim();
+
+      if (trimUserName === "") {
+        setValidateName(true);
+        return;
+      }
+
+      dispatch(addNewName(trimUserName));
+    }
+  };
 
   return (
     <Tile>
       <TileTitle>Ustawienia imienia</TileTitle>
       <p>
-        Twoje obecne imię to <b>{userName}</b>
+        Twoje obecne imię to <b>{userNameState}</b>
       </p>
-      <Form>
-        <InputText type="text" placeholder="Podaj nowe imię" />
+      <Form onSubmit={onFromSubmit}>
+        <InputName
+          type="text"
+          ref={userName}
+          maxLength={10}
+          $validName={validateName}
+          placeholder="Podaj nowe imię"
+        />
         <ButtonConfirm type="submit">Zmień imię</ButtonConfirm>
       </Form>
       <Description>
--- src/features/Settings/styled.ts
+++ src/features/Settings/styled.ts
@@ -1,6 +1,31 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { InputText } from "../../common/Input/styled";
+
+interface ValidateName {
+  $validName: boolean;
+}
 
 export const Form = styled.form`
   display: flex;
   gap: 15px;
+`;
+
+export const InputName = styled(InputText)<ValidateName>`
+  max-width: 200px;
+
+  ${({ $validName }) =>
+    $validName &&
+    css`
+      border-color: ${({ theme }) => theme.color.borderInputError};
+      box-shadow: 0 0 5px ${({ theme }) => theme.color.borderInputError};
+
+      &:hover,
+      &:focus {
+        box-shadow: none;
+      }
+    `}
+
+  @media (max-width: ${({ theme }) => theme.media.height350}px) {
+    max-width: 100%;
+  }
 `;
