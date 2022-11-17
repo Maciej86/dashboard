f901fe373bafe108a9e19e7cdf6af918c2d1bd71
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-17 23:56:11 +0100

    Added dispatch Confirm

--- src/features/Confirm/index.tsx
+++ src/features/Confirm/index.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectConfirm } from "./sliceConfirm";
 import { Wrapper, Popup, Strap, Text } from "./styled";
 
 export const Confirm = () => {
-  return (
-    <Wrapper>
-      <Popup>
-        <Strap></Strap>
-        <Text>Imię zostało zmienione</Text>
-      </Popup>
-    </Wrapper>
-  );
+  const info = useSelector(selectConfirm);
+  const [popupArray, setPopupArray] = useState<any>([]);
+
+  useEffect(() => {
+    info.id !== ""
+      ? setPopupArray(
+          popupArray.concat(
+            <Popup key={info.id}>
+              <Strap $status={info.status}></Strap>
+              <Text>{info.textConfirm}</Text>
+            </Popup>
+          )
+        )
+      : setPopupArray([]);
+  }, [info]);
+
+  console.log(popupArray);
+
+  return <Wrapper>{popupArray}</Wrapper>;
 };
