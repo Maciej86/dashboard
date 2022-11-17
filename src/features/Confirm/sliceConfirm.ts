db5e73928d48a5cccafcd02c8a985c7103be221f
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-17 23:10:43 +0100

    Added slice Confirm

---
+++ src/features/Confirm/sliceConfirm.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../core/store";
+
+interface ConfirmState {
+  status: string;
+  textConfirm: string;
+  id: string;
+}
+
+const initialConfirm: ConfirmState = {
+  status: "",
+  textConfirm: "",
+  id: "",
+};
+
+const confirmSlice = createSlice({
+  name: "confirm",
+  initialState: initialConfirm,
+  reducers: {
+    addConfirm: (state, action: PayloadAction<ConfirmState>) => {
+      state.status = action.payload.status;
+      state.textConfirm = action.payload.textConfirm;
+      state.id = action.payload.id;
+    },
+  },
+});
+
+export const selectConfirm = (state: RootState) => state.confirmStore;
+
+export const { addConfirm } = confirmSlice.actions;
+
+export default confirmSlice.reducer;
