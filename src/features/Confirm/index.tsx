80e404f2b585b36890d58810a68d3a861c6cd988
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-18 21:36:11 +0100

    Removing unnecessary import

--- src/features/Confirm/index.tsx
+++ src/features/Confirm/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeConfirm, selectConfirmState } from "./sliceConfirm";
 import { Wrapper, Popup, Strap, Text } from "./styled";
