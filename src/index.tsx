8ba4b468de2047c86633242c181cc7bbb83770a1
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-18 23:29:14 +0100

    Delete HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { HashRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import store from "./core/store";
 import { RouterSetings } from "./core/settingsNavigation";
 import { Normalize } from "styled-normalize";
@@ -13,13 +13,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <Normalize />
-        <GlobalStyle />
-        <RouterProvider router={RouterSetings} />
-      </Provider>
-    </HashRouter>
+    <Provider store={store}>
+      <Normalize />
+      <GlobalStyle />
+      <RouterProvider router={RouterSetings} />
+    </Provider>
   </React.StrictMode>
 );
 
