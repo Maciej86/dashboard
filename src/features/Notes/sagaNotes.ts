4eb5bb5c02738fc18fdddaa5e713914ff71536c2
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-22 20:48:40 +0100

    Save notes in localStorage

--- src/features/Notes/sagaNotes.ts
+++ src/features/Notes/sagaNotes.ts
@@ -2,11 +2,11 @@ import { call, takeEvery, select } from "redux-saga/effects";
 import { saveDataInLocalStorage } from "../../core/saveLocalStorage";
 import { addNotes, selectNotes } from "./sliceNotes";
 
-function* addNotesHendler() {
+function* saveNotesHendler() {
   const notes: string = yield select(selectNotes);
   yield call(saveDataInLocalStorage, notes, "notesStore");
 }
 
 export function* notesSaga() {
-  yield takeEvery(addNotes.type, addNotesHendler);
+  yield takeEvery("*", saveNotesHendler);
 }
