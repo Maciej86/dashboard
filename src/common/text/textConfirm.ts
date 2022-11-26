70adb0d50dbbf83e468d71310c9662b5c6b549ff
Maciej Rościszewski <maciejrosciszewski@gmail.com>
2022-11-26 20:24:33 +0100

    Created file text confirm

---
+++ src/common/text/textConfirm.ts
@@ -0,0 +1,6 @@
+export const enum TextConfirm {
+  NAME_ADD = "Imię zostało zapisane",
+  NAME_EMPTY = "Nie podano imienia",
+  NOTE_ADD = "Notatka zostałą zapisana",
+  NOTE_DELETE = "Notatka została usunięta",
+}
